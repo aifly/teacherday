578e3c13940bdce9a8c280dde7d498ccd088e325
aifly <aifly_xc@163.com>
2017-09-10 13:38:52 +0800

    add all statistic

--- index.jsx
+++ index.jsx
@@ -17,7 +17,7 @@ import ZmitiInputApp from './input/index.jsx'
 import ZmitiResultApp from './result/index.jsx'
 
 var obserable = new Obserable();
-var worksid = '1797800906';
+var worksid = '1606979000';
 var data = {
 	wxappid: 'wxec2401ee9a70f3d9',
 	wxappsecret: 'fc2c8e7c243da9e8898516fa5da8cbbb'
@@ -34,6 +34,42 @@ class App extends Component {
 		this.viewW = document.documentElement.clientWidth;
 		this.viewH = document.documentElement.clientHeight;
 		window.s = this;
+		this.zmitiMap = [
+				{"name":"北京市", "log":"116.46", "lat":"39.92"},
+				{"name":"上海市", "log":"121.48", "lat":"31.22"},
+				{"name":"天津市", "log":"117.2", "lat":"39.13"},
+				{"name":"重庆市", "log":"106.54", "lat":"29.59"},
+				{"name":"石家庄", "log":"114.48", "lat":"38.03"},
+				{"name":"太原市", "log":"112.53", "lat":"37.87"},
+				{"name":"沈阳市", "log":"123.38", "lat":"41.8"},
+				{"name":"长春市", "log":"125.35", "lat":"43.88"},
+				{"name":"哈尔滨市", "log":"126.63", "lat":"45.75"},
+				{"name":"杭州市", "log":"120.19", "lat":"30.26"},
+				{"name":"福州市", "log":"119.3", "lat":"26.08"},
+				{"name":"济南市", "log":"106.54", "lat":"29.59"},
+				{"name":"郑州市", "log":"113.65", "lat":"34.76"},
+				{"name":"武汉市", "log":"114.31", "lat":"30.52"},
+				{"name":"长沙市", "log":"113", "lat":"28.21"},
+				{"name":"广州市", "log":"113.23", "lat":"23.16"},
+				{"name":"海口市", "log":"110.35", "lat":"20.02"},
+				{"name":"成都市", "log":"104.06", "lat":"30.67"},
+				{"name":"贵阳市", "log":"106.71", "lat":"26.57"},
+				{"name":"昆明市", "log":"102.73", "lat":"25.04"},
+				{"name":"南昌市", "log":"115.89", "lat":"28.68"},
+				{"name":"西安市", "log":"108.95", "lat":"34.27"},
+				{"name":"西宁市", "log":"101.74", "lat":"36.56"},
+				{"name":"兰州市", "log":"103.73", "lat":"36.03"},
+				{"name":"南宁市", "log":"106.54", "lat":"29.59"},
+				{"name":"乌鲁木齐市", "log":"87.68", "lat":"43.77"},
+				{"name":"呼和浩特市", "log":"111.65", "lat":"40.82"},
+				{"name":"拉萨市", "log":"91.11", "lat":"29.97"},
+				{"name":"银川市", "log":"106.27", "lat":"38.47"},
+				{"name":"台北市", "log":"121.5", "lat":"25.14"},
+				{"name":"香港", "log":"114.17", "lat":"22.27"},
+				{"name":"澳门", "log":"113.33", "lat":"22.13"},
+				{"name":"合肥市", "log":"117.27", "lat":"31.86"},
+				{"name":"南京市", "log":"118.78", "lat":"32.04"}
+			]
 	}
 	render() {
 
@@ -181,6 +217,64 @@ class App extends Component {
 					s.state.nickname = s.nickname;
 					window.nickname = s.nickname;
 					s.forceUpdate();
+
+					$.ajax({
+						url: 'http://api.zmiti.com/v2/works/update_pvnum/',
+						data: {
+							worksid: worksid
+						},
+						success(data){
+							if (data.getret === 0) {
+							}
+						}
+					});
+
+					var idx = Math.random()*s.zmitiMap.length|0;
+
+					$.ajax({
+							url:'http://api.zmiti.com/v2/weixin/save_userview/',
+							type:'post',
+							data:{
+								worksid:worksid,
+								wxopenid:s.openid,
+								wxname:s.nickname,
+								usercity:s.zmitiMap[idx].name,
+								longitude:s.zmitiMap[idx].log,
+								latitude:s.zmitiMap[idx].lat
+							}
+						}).done((data)=>{
+							if(data.getret === 0 ){
+								
+							}else{
+								//alert('save_userview getret : '+ data.getret +' msg : '+ data.getmsg)
+							}
+						},()=>{
+							//alert('save_userview error');
+						})
+
+					 
+					   	//获取用户积分
+						//
+						var opt = {
+					   		type:'map',
+					   		address:s.zmitiMap[idx].name,
+					   		pos:[s.zmitiMap[idx].log,s.zmitiMap[idx].lat],
+					   		nickname:s.nickname,
+					   		headimgurl:s.headimgurl
+					   	}
+				   		$.ajax({
+							url:'http://api.zmiti.com/v2/msg/send_msg/',
+							type:'post',
+							data:{
+								type:worksid,
+								content:JSON.stringify(opt),
+								to:opt.to||''
+							},
+							success(data){
+								
+								//console.log(data);
+							}
+						})
 
 				} else {
 					if (s.isWeiXin()) {
